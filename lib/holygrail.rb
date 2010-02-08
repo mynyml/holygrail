d1996e1969e2a1729ea5d3b14db5e20a93d685cd
mynyml <mynyml@gmail.com>
2010-02-08 00:28:25 -0500

    Update inline docs

--- lib/holygrail.rb
+++ lib/holygrail.rb
@@ -7,6 +7,8 @@ module ActionController
       # Clear harmony page on every request.
       # Prevents changes to context from bleeding into the next one.
       #
+      # @example
+      #
       #     get :index
       #     js("foo = 'bar'")
       #     js('foo') #=> "bar"
@@ -14,13 +16,29 @@ module ActionController
       #     get :index
       #     js('foo') #=> "Error: foo is not defined"
       #
+      # @private
+      #
       def process(*args) #:nodoc:
         @__page = nil
         super
       end
 
+      # Execute javascript within the context of a view.
+      #
+      # @example
+      #
+      #     class PeopleControllerTest < ActionController::TestCase
+      #       get :index
+      #       assert_equal 'People: index', js('document.title')
+      #     end
+      #
+      # @param [String] code javascript code to evaluate
+      # @raise [Johnson::Error] javascript code exception
+      # @return [Object] return value of last javascript statement, cast to an
+      #   equivalent ruby object
+      #
       def execute_javascript(code)
-        @__page ||= Harmony::Page.new(@response.body)
+        @__page ||= Harmony::Page.new(@response.body.to_s)
         @__page.execute_js(code)
       end
       alias :js :execute_javascript
